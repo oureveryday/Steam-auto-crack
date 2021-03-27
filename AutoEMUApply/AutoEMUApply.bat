a74383f89bdae7f85d8aefd559c7b4e9218b991f
oureveryday <oureveryday@163.com>
2021-03-27 10:19:40 +0800

    Update goleberg EMU to 44305a00 Updated file folder

--- AutoEMUApply/AutoEMUApply.bat
+++ AutoEMUApply/AutoEMUApply.bat
@@ -13,14 +13,14 @@ goto end
 :api64
 echo steam_api64.dll found.
 move /Y %1\steam_api64.dll %1\steam_api64.dll.bak >nul 2>nul
-copy %~dp0Goldberg\steam_api64.dll %1\steam_api64.dll >nul 2>nul
+copy %~dp0..\Goldberg\steam_api64.dll %1\steam_api64.dll >nul 2>nul
 xcopy %~dp0..\Temp\steam_settings\ %1\steam_settings\ /E /C /Q /H /R /Y >nul 2>nul
 goto detect
 
 :api
 echo steam_api.dll found.
 move /Y %1\steam_api.dll %1\steam_api.dll.bak >nul 2>nul
-copy %~dp0Goldberg\steam_api.dll %1\steam_api.dll >nul 2>nul
+copy %~dp0..\Goldberg\steam_api.dll %1\steam_api.dll >nul 2>nul
 xcopy %~dp0..\Temp\steam_settings\ %1\steam_settings\ /E /C /Q /H /R /Y >nul 2>nul
 
 goto end
--- FindAPIApplyModule/FindAPIApplyModule.bat
+++ FindAPIApplyModule/FindAPIApplyModule.bat
@@ -12,7 +12,7 @@ for /f "tokens=1* delims=" %%i in (%~dp0..\Temp\Foundfile.txt) do (
     set a="%%i%%j"
     echo Found !a!,Applying......
     move /Y !a! !a!.bak >nul 2>nul
-    copy %~dp0Goldberg\steam_api64.dll !a! >nul 2>nul
+    copy %~dp0..\Goldberg\steam_api64.dll !a! >nul 2>nul
     echo copy config to %%~dpi......
     cd /d %~dp0
     xcopy %~dp0..\Temp\steam_settings\ "%%~dpi\steam_settings\" /E /C /Q /H /R /Y >nul 2>nul
@@ -25,7 +25,7 @@ for /f "tokens=1* delims=" %%i in (%~dp0..\Temp\Foundfile.txt) do (
     set a="%%i%%j"
     echo Found !a!,Applying......
     move /Y !a! !a!.bak >nul 2>nul
-    copy %~dp0Goldberg\steam_api.dll !a! >nul 2>nul
+    copy %~dp0..\Goldberg\steam_api.dll !a! >nul 2>nul
     echo copy config to %%~dpi......
     cd /d %~dp0
     xcopy %~dp0..\Temp\steam_settings\ %%~dpi\steam_settings\ /E /C /Q /H /R /Y >nul 2>nul
